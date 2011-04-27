1da8650bbdcaec4406c49ff671b19ec7e141a6d6
Yann Thomas-Gerard <inside@gmail.com>
2011-04-27 15:57:34 +0200

    Implements the Runnable interface allowing threaded animation

--- src/com/github/inside/Board.java
+++ src/com/github/inside/Board.java
@@ -1,15 +1,59 @@
 package com.github.inside;
 
 import java.awt.Color;
-import javax.swing.border.*;
-import javax.swing.BorderFactory;
+import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.Toolkit;
 
-public class Board extends JPanel
+public class Board extends JPanel implements Runnable
 {
+    private int x = 0;
+    private int y = 0;
+    private Thread thread;
+    public boolean isRunning = false;
+
     public Board()
     {
-        this.setBorder(BorderFactory.createLineBorder(Color.GREEN, 5));
+        this.setDoubleBuffered(true);
         this.setBackground(Color.BLACK);
+    }
+
+    public void addNotify()
+    {
+        super.addNotify();
+        this.thread = new Thread(this);
+        this.thread.start();
+    }
+
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        g.setColor(Color.RED);
+        g.fillRect(this.x, this.y, 10, 10);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    public void cycle()
+    {
+        this.x += 10;
+        this.y += 10;
+    }
+
+    public void run()
+    {
+        while (true)
+        {
+            this.cycle();
+            this.repaint();
+
+            try
+            {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("interrupted");
+            }
+        }
     }
 }

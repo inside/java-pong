307a07fe6658068b89a232d91c89eff9943a2fbe
Yann Thomas-Gerard <inside@gmail.com>
2011-04-28 17:32:18 +0200

    wip on key handling

--- src/com/github/inside/Board.java
+++ src/com/github/inside/Board.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import com.github.inside.Config;
 
 public class Board extends JPanel implements Runnable
@@ -15,16 +17,41 @@ public class Board extends JPanel implements Runnable
 
     public Board()
     {
+        this.setFocusable(true);
         this.setDoubleBuffered(true);
         this.setBackground(Color.BLACK);
+        this.addKeyListener(new KeyAdapter()
+        {
+            public void keyPressed(KeyEvent event)
+            {
+                int keyCode = event.getKeyCode();
+
+                System.out.println(keyCode);
+                if (keyCode == KeyEvent.VK_SPACE)
+                {
+                    System.out.println("space bar was pressed");
+                }
+            }
+
+            public void keyReleased(KeyEvent event)
+            {
+                int keyCode = event.getKeyCode();
+
+                System.out.println(keyCode);
+                if (keyCode == KeyEvent.VK_SPACE)
+                {
+                    System.out.println("space bar was released");
+                }
+            }
+        });
     }
 
-    public void addNotify()
-    {
-        super.addNotify();
-        this.thread = new Thread(this);
-        this.thread.start();
-    }
+//    public void addNotify()
+//    {
+//        super.addNotify();
+//        this.thread = new Thread(this);
+//        this.thread.start();
+//    }
 
     public void paint(Graphics g)
     {

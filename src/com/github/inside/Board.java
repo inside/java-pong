5c95c5ec9fa0007ba13fda68d45d99ed5573f76a
Yann Thomas-Gerard <inside@gmail.com>
2011-05-02 17:58:11 +0200

    Plays with types, implements start, stop, pause, pursue methods

--- src/com/github/inside/Board.java
+++ src/com/github/inside/Board.java
@@ -5,20 +5,23 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
 import com.github.inside.Config;
-//import java.util.Random;
 import java.lang.Math;
 
 public class Board extends JPanel implements Runnable
 {
-    private int x = 250;
-    private int y = 125;
+    private int x = 0;
+    private int y = 0;
     private Thread thread;
     private boolean started = false;
     private boolean paused = false;
     public boolean isRunning = false;
+    private long period = 0;
+    public long startTime = 0;
+    public long time = 0;
 
     public Board()
     {
+        this.period = Math.round((long) 1000 / Config.FRAME_RATE);
         this.setFocusable(true);
         this.setDoubleBuffered(true);
         this.setBackground(Color.BLACK);
@@ -33,10 +36,16 @@ public class Board extends JPanel implements Runnable
         Toolkit.getDefaultToolkit().sync();
     }
 
-    public void cycle()
+    public void updateForNewFrame()
     {
-        this.x += Math.random() * 2 - 1;
-        this.y += Math.random() * 2 - 1;
+        this.time = System.currentTimeMillis() - this.startTime;
+        this.startTime = System.currentTimeMillis();
+
+        this.x += (int) (Config.BALL_INITIAL_SPEED * (double) this.time);
+        this.y += (int) (Config.BALL_INITIAL_SPEED * (double) this.time);
+        System.out.println(Config.BALL_INITIAL_SPEED);
+        System.out.println(this.time);
+        System.out.println((double) this.time);
         System.out.println(this.x);
     }
 
@@ -44,12 +53,12 @@ public class Board extends JPanel implements Runnable
     {
         while (true)
         {
-            this.cycle();
+            this.updateForNewFrame();
             this.repaint();
 
             try
             {
-                Thread.sleep(33);
+                Thread.sleep(this.getNextFrameDelay());
             }
             catch (InterruptedException e)
             {
@@ -66,15 +75,61 @@ public class Board extends JPanel implements Runnable
             return;
         }
 
-        this.started = true;
-        this.thread = new Thread(this);
-        this.thread.setPriority(Thread.MIN_PRIORITY);
-        this.thread.start();
-
+        this.stop();
+        this.pursue();
         System.out.println("start called");
     }
 
     public void stop()
+    {
+        this.started = false;
+        this.paused  = true;
+        this.stopThread();
+        System.out.println("stop called");
+    }
+
+    public void pause()
+    {
+        this.started = true;
+        this.paused  = true;
+        this.stopThread();
+    }
+
+    public void pursue()
+    {
+        this.started = true;
+        this.paused = false;
+        this.startTime = System.currentTimeMillis();
+//        this.initFireProjectileTime();
+        this.startThread();
+        System.out.println("pursue called");
+    }
+
+    public void togglePause()
+    {
+        if (!this.started)
+        {
+            return;
+        }
+        if (this.paused)
+        {
+            this.pursue();
+        }
+        else
+        {
+            this.pause();
+        }
+        System.out.println("togglePause called");
+    }
+
+    private void startThread()
+    {
+        this.thread = new Thread(this);
+        this.thread.setPriority(Thread.MIN_PRIORITY);
+        this.thread.start();
+    }
+
+    private void stopThread()
     {
         if (this.thread != null)
         {
@@ -82,12 +137,17 @@ public class Board extends JPanel implements Runnable
         }
 
         this.thread = null;
-        this.started = false;
-        System.out.println("stop called");
     }
 
-    public void togglePause()
+    private long getNextFrameDelay()
     {
-        System.out.println("togglePause called");
+        long processingTime = System.currentTimeMillis() - this.startTime;
+
+        if (this.period < processingTime)
+        {
+            return 0;
+        }
+
+        return this.period - processingTime;
     }
 }
--- src/com/github/inside/Config.java
+++ src/com/github/inside/Config.java
@@ -2,29 +2,29 @@ package com.github.inside;
 
 class Config
 {
-    public static final short FRAME_RATE = 60; // 30 to 100 images per second
-    public static final short MAX_SCORE = 50;
-    public static final short PONG_WIDTH = 500;
-    public static final short PONG_HEIGHT = 250;
+    public static final int FRAME_RATE = 60; // 30 to 100 images per second
+    public static final int MAX_SCORE = 50;
+    public static final int PONG_WIDTH = 500;
+    public static final int PONG_HEIGHT = 250;
 
-    public static final float PADDLE_INITIAL_SPEED = 0.2F;
-    public static final float PADDLE_MIN_SPEED = 0.1F;
-    public static final float PADDLE_MAX_SPEED = 0.5F;
-    public static final short PADDLE_INITIAL_WIDTH = 6;
-    public static final short PADDLE_INITIAL_HEIGHT = 60;
-    public static final short PADDLE_MIN_HEIGHT = 30;
-    public static final short PADDLE_MAX_HEIGHT = 90;
+    public static final double PADDLE_INITIAL_SPEED = 0.2;
+    public static final double PADDLE_MIN_SPEED = 0.1;
+    public static final double PADDLE_MAX_SPEED = 0.5;
+    public static final int PADDLE_INITIAL_WIDTH = 6;
+    public static final int PADDLE_INITIAL_HEIGHT = 60;
+    public static final int PADDLE_MIN_HEIGHT = 30;
+    public static final int PADDLE_MAX_HEIGHT = 90;
 
-    public static final float BALL_INITIAL_SPEED = 0.2F;
-    public static final short BALL_INITIAL_WIDTH = 10;
-    public static final short BALL_INITIAL_HEIGHT = 10;
+    public static final double BALL_INITIAL_SPEED = 0.2;
+    public static final int BALL_INITIAL_WIDTH = 10;
+    public static final int BALL_INITIAL_HEIGHT = 10;
 
-    public static final float POWER_PROJECTILE_INITIAL_SPEED = 0.05F;
-    public static final short POWER_PROJECTILE_INITIAL_WIDTH = 10;
-    public static final short POWER_PROJECTILE_INITIAL_HEIGHT = 10;
+    public static final double POWER_PROJECTILE_INITIAL_SPEED = 0.05;
+    public static final int POWER_PROJECTILE_INITIAL_WIDTH = 10;
+    public static final int POWER_PROJECTILE_INITIAL_HEIGHT = 10;
 
-    public static final short POWER_LIFETIME = 10000; // milliseconds
+    public static final int POWER_LIFETIME = 10000; // milliseconds
 
-    public static final short MAX_PROJECTILES = 10;
-    public static final short PROJECTILES_DEFAULT_LIFETIME = 20000; // milliseconds
+    public static final int MAX_PROJECTILES = 10;
+    public static final int PROJECTILES_DEFAULT_LIFETIME = 20000; // milliseconds
 }

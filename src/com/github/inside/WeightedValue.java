f282701653a2f56982601c10c007ed55a91b4156
Yann Thomas-Gerard <inside@gmail.com>
2012-02-21 17:58:01 +0100

    Adds the weighted values container class

---
+++ src/com/github/inside/WeightedValue.java
@@ -0,0 +1,13 @@
+package com.github.inside;
+
+class WeightedValue
+{
+    public String name;
+    public int weight;
+
+    public WeightedValue(String name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+}

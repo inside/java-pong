97d0ca4d6995d112934650d523c02315bea9d6e4
Yann Thomas-Gerard <inside@gmail.com>
2012-02-21 17:57:36 +0100

    Implements the Helper class

--- src/com/github/inside/Helper.java
+++ src/com/github/inside/Helper.java
@@ -1,6 +1,39 @@
 package com.github.inside;
 
+import java.lang.Integer;
+
 class Helper
 {
-//    public getTime
+    public static int getRandomFromRange(int min, int max)
+    {
+        return min + (int) Math.round(Math.random() * (max - min));
+    }
+
+    public static String getWeightedRandomValue(WeightedValue[] weightedValues)
+    {
+        int weightSum = 0;
+        int limit = 0;
+        int random;
+
+        for (int i = 0; i < weightedValues.length; i++)
+        {
+            weightSum += weightedValues[i].weight;
+        }
+
+        random = Helper.getRandomFromRange(0, weightSum);
+
+        int i = 0;
+
+        for (; i < weightedValues.length; i++)
+        {
+            limit += weightedValues[i].weight;
+
+            if (limit >= random)
+            {
+                break;
+            }
+        }
+
+        return weightedValues[i].name;
+    }
 }
